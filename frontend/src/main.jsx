930898c72aee0f136e83e9ab700549ee3e189920
ripuranjan-143 <ripuranjan143@gmail.com>
2026-01-04 17:18:51 +0530

    feat(ui): add global toast notifications

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import ProjectRoutes from './ProjectRoutes.jsx';
+import GlobalToaster from './components/common/GlobalToaster.jsx';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')).render(
   <>
+    <GlobalToaster />
     <BrowserRouter>
       <ProjectRoutes />
     </BrowserRouter>
