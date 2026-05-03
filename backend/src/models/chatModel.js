ef706f8d4939ff2fe8450a09297312ff94a6bb9d
ripuranjan-143 <ripuranjan143@gmail.com>
2026-05-03 08:53:37 +0530

    fix(models): set Cloudinary URL as default avatar for user and chat

--- backend/src/models/chatModel.js
+++ backend/src/models/chatModel.js
@@ -26,8 +26,7 @@ const chatSchema = new mongoose.Schema(
     },
     picture: {
       type: String,
-      default:
-        'https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=',
+      default: 'https://res.cloudinary.com/dwv10qvzj/image/upload/v1777778232/avatar_wm3pfk.jpg',
     },
   },
   { timestamps: true }
